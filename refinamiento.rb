9e71f963cd6f348c8ea233432f29f2b624df36c7
Fernando Rubilar Z <frubilar@icci.cl>
2016-12-19 14:57:41 -0300

    CHG: Cambios  de calculo de angulos
    Signed-off-by: Fernando Rubilar Z <frubilar@icci.cl>

--- refinamiento.rb
+++ refinamiento.rb
@@ -41,17 +41,21 @@ end
 
 
 def angulosTriangulos(a,b,c)
-  angulo = 57.2958
-  alfa = (angulo)*Math.acos(((c**2)+(b**2)-(a**2))/(2*b*c))
-  beta = (angulo)*Math.acos(((a**2)+(c**2)-(b**2))/(2*a*c))
-  gama= (angulo)*Math.acos(((a**2)+(b**2)-(c**2))/(2*a*b))
+  angulo = 180/Math::PI
+  bCuadrado= b*b
+  cCuadrado = c*c
+  aCuadrado = a*a
+  alfa = Math.acos((bCuadrado+cCuadrado- aCuadrado)/(2*b*c))* angulo
+  beta = Math.acos((aCuadrado+cCuadrado- bCuadrado )/(2*a*c))*angulo
+  gama = Math.acos((aCuadrado+bCuadrado-cCuadrado)/(2*a*b))*angulo
   refinar = 0
   if (alfa <= 18 || beta <= 18 || gama <= 18)
     refinar = 1
   end
-#  puts (alfa).to_s+"---"+(beta).to_s+"  ---- "+(gama).to_s
+  puts (alfa).to_s+"---"+(beta).to_s+"  ---- "+(gama).to_s
   return refinar
 end
+
 
 def cantidadDeTriangulos(mesh)
   largo = mesh[0][0]
@@ -59,12 +63,27 @@ def cantidadDeTriangulos(mesh)
 end
 
 
-listaa =[]
-listab=[]
-listac=[]
+def angulo(a,b,c)
+  gama=Math.acos((c*c-a*a-b*b)/(-2*a*b))*180/Math::PI
+  alfa=Math.acos((a*a-b*b-c*c)/(-2*b*c))*180/Math::PI
+  beta=Math.acos((b*b-a*a-c*c)/(-2*a*c))*180/Math::PI
+
+  refinar = 0
+  if (alfa <= 18 || beta <= 18 || gama <= 18)
+    refinar = 1
+  end
+  puts (alfa).to_s+"---"+(beta).to_s+"  ---- "+(gama).to_s
+  return refinar
+
+end
+
 largo= cantidadDeTriangulos(mesh)
 lista =[]
+segunda = []
 for j in 1..largo
+  a=0
+  b=0
+  c=0
   for i in 0..node.length-1
     if mesh[j][0]==node[i][0]
       largo1 = node[i]
